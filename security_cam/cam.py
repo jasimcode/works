5a909bb0302ac46da25068c5f97b1713ba1ee24c
Jasim <122201959+jasimcode@users.noreply.github.com>
2023-01-08 14:41:45 +0530

    Add files via upload

---
+++ security_cam/cam.py
@@ -0,0 +1,10 @@
+import cv2
+cam = cv2.VideoCapture(1)
+while cam.isOpened():
+    ret, frame1 = cam.read()
+    '''ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
+    if cv2.waitKey(10) == ord('q'):
+        break
+        '''
+cv2.imshow('Granny Cam', frame1)
